6adb5d417c895e77ba757fd80f2b4f3cafe96e7d
Joshua Viera <joshuaviera95@gmail.com>
2025-10-12 14:49:39 -0400

    Initial Commit

---
+++ ForYouHub/ForYouHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForYouHubApp.swift
+//  ForYouHub
+//
+//  Created by Joshua Viera on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ForYouHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForYouHubTests/ForYouHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  ForYouHubTests.swift
+//  ForYouHubTests
+//
+//  Created by Joshua Viera on 10/12/25.
+//
+
+import Testing
+
+struct ForYouHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
